4f53d8b5dd99b973d6abce9164b64c2ec9d5931d
1cu2nm2 <98898898+1cu2nm2@users.noreply.github.com>
2022-02-04 17:01:35 +0800

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,40 @@
+Profingh= /* CommendusProfingh */
+case /* value */:
+for (size_t i = 0; i < count; i++) {
+  /* code */
+}
+B["void name(/* arguments */) {
+  /* code */
+}"]
+{geming'void name(/* arguments */) {
+  /* code */
+}}
+=#ifndef 1
+#define 1 value
+#endif
+printf("%s\n", );
+case /* value */:
+/*Programm]=Exe.='Probing.if (/* condition */) {
+  /* code */
+} else if (/* condition */) {
+  /* code */
+} else {
+  /* code */
+}'*/
+/* condition valve*/
+void Probing.exe(/* arguments */) {
+  /* code */
+}{"condition"case /* value */:}
+
+
+
+
+}{"condition"switch (/* expression */) {
+  case /* value */:
+}}"Bowingos"
+/* code ScriptProbing*/
+#ifndef SYMBOL
+#define  value
+#endifcs
+
+cs
